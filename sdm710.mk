50fded30fee223b0a475eb38b78f475f4ace2c24
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2020-02-18 21:22:19 +0530

    RMX199X: Convert to RealmeParts

--- sdm710.mk
+++ sdm710.mk
@@ -99,6 +99,10 @@ PRODUCT_PACKAGES += \
     rcs_service_api \
     rcs_service_api.xml
 
+# RealmeParts
+PRODUCT_PACKAGES += \
+    RealmeParts
+
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
